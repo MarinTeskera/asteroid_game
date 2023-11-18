d4d831823e4c9734e5f884adf5cf71737c54c508
Marin Teskera <marin.teskera@infinum.com>
2023-11-18 18:34:46 +0100

    add border

--- game.js
+++ game.js
@@ -43,8 +43,12 @@ export class Game {
   }
 
   draw() {
-    this.ctx.fillStyle = "black"; // Set canvas background color to black
-    this.ctx.fillRect(0, 0, this.width, this.height);
+    // this.ctx.fillStyle = "black"; // Set canvas background color to black
+    // this.ctx.fillRect(0, 0, this.width, this.height);
+
+    this.ctx.strokeStyle = "green";
+    this.ctx.lineWidth = 5;
+    this.ctx.strokeRect(0, 0, this.width, this.height);
 
     this.player.draw(this.ctx);
     this.asteroids.forEach((asteroid) => asteroid.draw(this.ctx));
