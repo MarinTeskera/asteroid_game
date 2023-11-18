53645eb7406b2dc732b0a45f0118a4bd24b750a2
Marin Teskera <marin.teskera@infinum.com>
2023-11-18 13:46:01 +0100

    random asteroid position and speed

--- asteroid.js
+++ asteroid.js
@@ -5,13 +5,55 @@ export class Asteroid {
     const size = Math.floor(Math.random() * (50 - 20 + 1)) + 20;
     this.width = size;
     this.height = size;
-    this.x = 0;
-    this.y = 0;
+    this.spawnOutsideScreen();
+  }
+
+  spawnOutsideScreen() {
+    const spawnSide = Math.floor(Math.random() * 4);
+
+    switch (spawnSide) {
+      case 0: // Top
+        this.x = Math.random() * this.game.width;
+        this.y = -this.height;
+        this.speedX = Math.random() * 2 - 1; // Random number between -1 and 1
+        this.speedY = Math.random(); // Only positive speedY
+        break;
+      case 1: // Right
+        this.x = this.game.width;
+        this.y = Math.random() * this.game.height;
+        this.speedX = -Math.random(); // Only negative speedX
+        this.speedY = Math.random() * 2 - 1; // Random number between -1 and 1
+        break;
+      case 2: // Bottom
+        this.x = Math.random() * this.game.width;
+        this.y = this.game.height;
+        this.speedX = Math.random() * 2 - 1; // Random number between -1 and 1
+        this.speedY = -Math.random(); // Only negative speedY
+        break;
+      case 3: // Left
+        this.x = -this.width;
+        this.y = Math.random() * this.game.height;
+        this.speedX = Math.random(); // Only positive speedX
+        this.speedY = Math.random() * 2 - 1; // Random number between -1 and 1
+        break;
+      default:
+        break;
+    }
   }
 
   update() {
-    this.x += 1;
-    this.y += 1;
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    // Check for collision with player
+    if (
+      this.x < this.player.x + this.player.width &&
+      this.x + this.width > this.player.x &&
+      this.y < this.player.y + this.player.height &&
+      this.y + this.height > this.player.y
+    ) {
+      console.log("Collision with player!");
+    }
   }
 
   draw(context) {
