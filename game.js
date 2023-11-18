5d4501fb29a0b6c2cb556761f8b52c7d703fa860
Marin Teskera <marin.teskera@infinum.com>
2023-11-18 14:04:04 +0100

    add timer

--- game.js
+++ game.js
@@ -8,10 +8,11 @@ export class Game {
     this.height = height;
     this.ctx = ctx;
     this.player = new Player(this, 5);
-    this.asteroids = [];
+    this.asteroids = [new Asteroid(this, this.player)];
     this.input = new InputHandler();
     this.asteroidSpawnInterval = 2000; // Interval in milliseconds
     this.lastSpawnTime = Date.now();
+    this.startTime = Date.now(); // Added timer start time
   }
 
   update() {
@@ -23,15 +24,32 @@ export class Game {
       this.spawnNewAsteroid();
       this.lastSpawnTime = Date.now();
     }
+
+    this.updateTimer();
   }
 
   draw() {
+    this.ctx.fillStyle = "black";
+    this.ctx.fillRect(0, 0, this.width, this.height);
     this.player.draw(this.ctx);
     this.asteroids.forEach((asteroid) => asteroid.draw(this.ctx));
+    this.drawTimer();
   }
 
   spawnNewAsteroid() {
     const newAsteroid = new Asteroid(this, this.player);
     this.asteroids.push(newAsteroid);
+  }
+
+  updateTimer() {
+    this.elapsedTime = Date.now() - this.startTime;
+  }
+
+  drawTimer() {
+    const minutes = Math.floor(this.elapsedTime / 60000);
+    const seconds = ((this.elapsedTime % 60000) / 1000).toFixed(2);
+    this.ctx.fillStyle = "white";
+    this.ctx.font = "20px Arial";
+    this.ctx.fillText(`Time: ${minutes}:${seconds}`, this.width - 200, 20);
   }
 }
